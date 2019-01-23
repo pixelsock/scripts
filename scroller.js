bf4edbec482ef65f4fc9422d8fa456a1468d76e6
pixelsock <nick@pixelsock.com>
2019-01-23 15:33:42 -0500

    Update scroller.js

--- scroller.js
+++ scroller.js
@@ -1,63 +1,543 @@
-(function ( $ ) {
-    var _ = {
-        lastScrollTop : 0,
-        direction: null,
-    };
-    $.fn.infinite = function(options) {
-        var defaults = {
-            container : '.js-loop',
-        };
+(function($)
+{
 
-        var o = $.extend( {}, defaults, options );
-        $.fn.infinite._o = o;
-        startLoop = function(){
-            $(window).on('scroll',  handler.scroll);
-            $.fn.infinite.clone();
-        };
-        $.fn.infinite.clone = function(){
-            var loop_clone = $(o.container).clone();
-            $(o.container).parent().prepend( loop_clone ).append( loop_clone );
-            setTimeout(function(){
-                $.fn.infinite.startPosition();
-            }, 200);
-        };
-        $.fn.infinite.startPosition = function(){
-            var center = $(o.container+':nth-child(2)').offset().top;
-            $(window).scrollTop(center); 
-        };
+	//Function scrollBottom() -> inverted scrollTop() function
+	
+	$.fn.scrollBottom=function(){
+		if($(this).is(document))
+			return $(this).height()-$(this).scrollTop()-$(window).height();
+		else
+			return $(this).prop('scrollHeight')-$(this).prop('offsetHeight')-$(this).scrollTop();
+	}
+	
+	//Function scrollRight() -> inverted scrollLeft() function
+	
+	$.fn.scrollRight=function(){
+		if($(this).is(document))
+			return $(this).width()-$(this).scrollLeft()-$(window).width();
+		else{
+			return $(this).prop('scrollWidth')-$(this).prop('offsetWidth')-$(this).scrollLeft();
+		}
+	}
+	
+	$.fn.endless=function(options){
+	
+		//null function
+		var nothing=function(){};
+	
+		//Default values
+		var defaults={
+			direction:'down',
+			append:nothing,
+			prepend:nothing,
+			n_append:nothing,
+			n_prepend:nothing
+		};
+		
+		//Settings
+		var settings=$.extend(defaults,options);
+	
+		//Variables used
+		var high=$(this).height();
+		var width=$(this).width();
+		var content;
+		var top_position=$(this).scrollTop();
+		var left_position;
+		
+		//if the object is document => change variables + encapsulate in element's html div
+		if($(this).is(document)){
+			left_position=$(this).scrollLeft();
+			if(settings.direction=='down'||settings.direction=='up'||settings.direction=='vertical'){
+				//if the element's height is smaller than his parent => add content or increase height
+				if($('body').height()<=$(window).height()){
+					content='<div class="endless" style="height:'+($(window).height()+2)+'px;">'+$('body').html()+'</div>';
+					$('body').css('height',$(window).height()+2);
+				}
+				else
+					content='<div class="endless">'+$('body').html()+'</div>';
+			}
+			else{
+				//if the element's width is smaller than his parent => add content or increase width
+				if($('body').width()<=$(window).width()){
+					content='<div class="endless" style="width:'+($(window).width()+2)+'px;">'+$('body').html()+'</div>';
+					$('body').css('width',$(window).width()+2);
+				}
+				else
+					content='<div class="endless">'+$('body').html()+'</div>';
+			}
+			$('body').html(content);
+		}
+		else{
+			left_position=$(this).parent().scrollLeft();
+			if(settings.direction=='down'||settings.direction=='up'||settings.direction=='vertical'){
+				//if the element's height is smaller than his parent => add content or increase height
+				if($(this).offset().top+$(this).height()<=$(this).parent().height())
+					content='<div class="endless" style="height:'+($(this).parent().height()+2)+'px;">'+$(this).html()+'</div>';
+				else
+					content='<div class="endless">'+$(this).html()+'</div>';
+			}
+			else{
+				//if the element's width is smaller than his parent => add content or increase width
+				if($(this).offset().left+$(this).width()<=$(this).parent().width())
+					content='<div class="endless" style="width:'+($(this).parent().width()+2)+'px;">'+$(this).html()+'</div>';
+				else
+					content='<div class="endless">'+$(this).html()+'</div>';
+			}
+			$(this).html(content);
+		}
+		
+		//Enable-Disable the scrollbar
+		if(settings.scrollbar&&(settings.scrollbar=='enable'||settings.scrollbar=='disable')){
+			if(settings.scrollbar=='disable'){
+				if(settings.direction=='down'||settings.direction=='up'||settings.direction=='vertical'){
+					if($(this).is(document))
+						$('body').css('overflow-y','hidden');
+					else
+						$(this).css('overflow-y','hidden');
+				}
+				else{
+					if($(this).is(document))
+						$('body').css('overflow-x','hidden');
+					else
+						$(this).parent().css('overflow-x','hidden');
+				}
+			}
+			else{
+				if(settings.direction=='right'||settings.direction=='left'||settings.direction=='horizontal'){
+					if($(this).is(document))
+						$('body').css('overflow-y','scroll');
+					else
+						$(this).css('overflow-y','scroll');
+				}
+				else{
+					if($(this).is(document))
+						$('body').css('overflow-x','scroll');
+					else
+						$(this).parent().css('overflow-x','scroll');
+				}
+			}
+		}
 
-
-
-        $.fn.infinite.destroy = function(){
-            $(window).off('scroll');
-            $(window).off('mousewheel');
-        }
-        $.fn.infinite.debug = function(){
-            console.log(defaults , options);
-        };
-
-        startLoop();
-    };
-
-    var handler = {
-        scroll : function(){
-            var down_clone    = $($.fn.infinite._o.container+':last-child '),
-                win_height    = $(window).height(),
-                win_width     = $(window).width(),
-                winPos        = $(window).scrollTop(),
-                clone_top     = down_clone.offset().top,
-                loop_height   = $($.fn.infinite._o.container).parent().outerHeight(true);
-
-
-                        if(win_height + winPos >= loop_height ){
-                var scroll_down_loop =  down_clone.outerHeight(true) - win_height + (win_width * .03); 
-                $(window).scrollTop(scroll_down_loop); 
-            }else if(winPos <= 0 ){
-                var scroll_up_loop =  $($.fn.infinite._o.container).last().offset().top ; 
-                $(window).scrollTop(scroll_up_loop); 
-            }
-
-        }
-    };
-    module.exports = $.fn.infinite;
-}(jQuery));
+		//Scroll direction
+		
+		//Boolean variables
+		var append=false;
+		var prepend=false;
+		
+		/* <!--------------------------------------- UP AND DOWN ----------------------------------------------------> */
+		
+		//Down
+		
+		if(settings.direction=='down'){
+		
+			$(this).scroll(function(){
+				//Scroll down
+				if(top_position<$(this).scrollTop()){
+					top_position=$(this).scrollTop();
+					if($(this).scrollBottom()==0){
+						if($(this).is(document))
+							$('body').append($('body').html());
+						else
+							$(this).append($(this).html());
+						$(this).scrollTop(top_position);
+						append=true;
+						settings.append.call();
+					}
+					else if($(this).scrollTop()>=high){
+						if($(this).is(document))
+							$('body').children('endless:first-child').remove();
+						else
+							$(this).children('.endless:first-child').remove();
+						$(this).scrollTop(1);
+						top_position=1;
+						append=false;
+						settings.n_prepend.call();
+					}
+				}
+				else if(append){
+					if($(this).scrollBottom()>=high){
+						if($(this).is(document)){
+							$('body').children('.endless:last-child').remove();
+							$(this).scrollTop(high-$(window).height()-1);
+							top_position=$(this).scrollTop();
+						}
+						else{
+							$(this).children('.endless:last-child').remove();
+							$(this).scrollTop(high-1);
+							top_position=high-1;
+						}
+						append=false;
+						settings.n_prepend.call();
+					}
+				}
+			});
+		}
+		
+		//Up
+		
+		else if(settings.direction=='up'){
+			$(this).scroll(function(){
+				if (top_position>=$(this).scrollTop()){
+					top_position=$(this).scrollTop();
+					if($(this).scrollTop()==0){
+						if($(this).is(document))
+							$('body').prepend($('body').html());
+						else
+							$(this).prepend($(this).html());
+						$(this).scrollTop(high);
+						top_position=$(this).scrollTop();
+						prepend=true;
+						settings.prepend.call();
+					}
+					else if($(this).scrollBottom()>=high){
+						if($(this).is(document)){
+							$('body').children('.endless:last-child').remove();
+							$(this).scrollTop(high-$(window).height()-1);
+							top_position=$(this).scrollTop();
+						}
+						else{
+							$(this).children('.endless:last-child').remove();
+							$(this).scrollTop(high-1);
+							top_position=high-1;
+						}
+						prepend=false;
+						settings.n_append.call();
+					}
+				}
+				else if(prepend){
+						if($(this).scrollTop()>=high){
+							if($(this).is(document))
+								$('body').children('.endless:first-child').remove();
+							else
+								$(this).children('.endless:first-child').remove();
+							$(this).scrollTop(1);
+							top_position=1;
+							prepend=false;
+							settings.n_append.call();
+						}
+				}
+			});
+		}
+		
+		//Up and Down (Vertical)
+		
+		else if(settings.direction=='vertical'){
+		
+			$(this).scroll(function(){
+				//Scroll down
+				if(top_position<$(this).scrollTop()){
+					top_position=$(this).scrollTop();
+					if($(this).scrollBottom()==0){
+						if($(this).is(document))
+							$('body').append($('body').html());
+						else
+							$(this).append($(this).html());
+						$(this).scrollTop(top_position);
+						append=true;
+						settings.append.call();
+					}
+					else if($(this).scrollTop()>=high){
+						if($(this).is(document))
+							$('body').children('.endless:first-child').remove();
+						else
+							$(this).children('.endless:first-child').remove();
+						$(this).scrollTop(1);
+						top_position=1;
+						prepend=false;
+						settings.n_prepend.call();
+					}
+				}
+				//Scroll up
+				else if(top_position>=$(this).scrollTop()){
+					top_position=$(this).scrollTop();
+					if($(this).scrollTop()==0){
+						if($(this).is(document))
+							$('body').prepend($('body').html());
+						else
+							$(this).prepend($(this).html());
+						$(this).scrollTop(high);
+						top_position=$(this).scrollTop();
+						prepend=true;
+						settings.prepend.call();
+					}
+					else if($(this).scrollBottom()>=high){
+						if($(this).is(document)){
+							$('body').children('.endless:last-child').remove();
+							$(this).scrollTop(high-$(window).height()-1);
+							top_position=$(this).scrollTop();
+						}
+						else{
+							$(this).children('.endless:last-child').remove();
+							$(this).scrollTop(high-1);
+							top_position=high-1;
+						}
+						append=false;
+						settings.n_append.call();
+					}
+				}
+			});
+		}
+		
+		/* <!--------------------------------------- LEFT AND RIGHT ----------------------------------------------------> */
+		
+		//Right
+		
+		if(settings.direction=='right'){
+			//if the object is document
+			if($(this).is(document)){
+				$(this).scroll(function(){
+					//Scroll right
+					if(left_position<$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollRight()==0){
+							$('body').css('width',$('body').width()*2);
+							$('body').children('.endless').css('float','left');
+							$('body').append($('body').html());
+							$('body').children('.endless:nth-child(2)').css('float','right');
+							$(this).scrollLeft(left_position);
+							append=true;
+							settings.append.call();
+						}
+						else if($(this).scrollLeft()>=width){
+							$('body').children('.endless:first-child').remove();
+							$('body').css('width',$('body').width()/2);
+							$(this).scrollLeft(1);
+							left_position=1;
+							append=false;
+							settings.n_prepend.call();
+						}
+					}
+					else if(append){
+						if($(this).scrollRight()>=width){
+							$('body').children('.endless:last-child').remove();
+							$('body').css('width',$('body').width()/2);
+							$(this).scrollLeft(width-$(window).width()-1);
+							left_position=$(this).scrollLeft();
+							append=false;
+							settings.n_prepend.call();
+						}
+					}
+				});
+			}
+			//else -> take the parent for scroll position
+			else{
+				$(this).parent().scroll(function(){
+					//Scroll right
+					if(left_position<$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollRight()==0){
+							$(this).children().css('width',$(this).children().children('.endless').width()*2);
+							$(this).children().children('.endless').css('float','left');
+							$(this).children().append($(this).children().html());
+							$(this).children().children('.endless:nth-child(2)').css('float','right');
+							$(this).scrollLeft(left_position);
+							append=true;
+							settings.append.call();
+						}
+						else if($(this).scrollLeft()>=width){
+							$(this).children().children('.endless:first-child').remove();
+							$(this).children().css('width',$(this).children().width()/2);
+							$(this).scrollLeft(1);
+							left_position=1;
+							append=false;
+							settings.n_prepend.call();
+						}
+					}
+					else if(append){
+						if($(this).scrollRight()>=0){
+							$(this).children().children('.endless:last-child').remove();
+							$(this).children().css('width',$(this).children().children('.endless').width());
+							$(this).scrollLeft(width-1);
+							left_position=width-1;
+							append=false;
+							settings.n_prepend.call();
+						}
+					}
+				});
+			}
+		}
+		
+		//Left
+		
+		else if(settings.direction=='left'){
+			//if the object is document
+			if($(this).is(document)){
+				$(this).scroll(function(){
+					//Scroll left
+					if(left_position>=$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollLeft()==0){
+							$('body').css('width',$('body').width()*2);
+							$('body').children('.endless').css('float','right');
+							$('body').prepend($('body').html());
+							$('body').children('.endless:first-child').css('float','left');
+							$(this).scrollLeft(width);
+							left_position=$(this).scrollLeft();
+							prepend=true;
+							settings.prepend.call();
+						}
+						else if($(this).scrollRight()>=width){
+							$('body').children('.endless:last-child').remove();
+							$('body').css('width',$('body').width()/2);
+							$(this).scrollLeft(width-$(window).width()-1);
+							left_position=$(this).scrollLeft();
+							prepend=false;
+							settings.n_append.call();
+						}
+					}
+					else if(prepend){
+							if($(this).scrollLeft()>=width){
+								$('body').children('.endless:first-child').remove();
+								$('body').css('width',$('body').width()/2);
+								$(this).scrollLeft(1);
+								left_position=1;
+								prepend=false;
+								settings.n_append.call();
+							}
+					}
+				});
+			}
+			//else -> take the parent for scroll position
+			else{
+				$(this).parent().scroll(function(){
+					//Scroll left
+					if(left_position>=$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollLeft()==0){
+							$(this).children().css('width',$(this).children().width()*2);
+							$(this).children().children('.endless').css('float','right');
+							$(this).children().prepend($(this).children().html());
+							$(this).children().children('.endless:first-child').css('float','left');
+							$(this).scrollLeft(width);
+							left_position=$(this).scrollLeft();
+							prepend=true;
+							settings.prepend.call();
+						}
+						else if($(this).scrollRight()>=width){
+							$(this).children().children('.endless:last-child').remove();
+							$(this).children().css('width',$(this).children().width()/2);
+							$(this).scrollLeft(width-1);
+							left_position=width-1;
+							prepend=false;
+							settings.n_append.call();
+						}
+					}
+					else if(prepend){
+							if($(this).scrollLeft()>=width){
+								$(this).children().children('.endless:first-child').remove();
+								$(this).children().css('width',$(this).children().width()/2);
+								$(this).scrollLeft(1);
+								left_position=1;
+								prepend=false;
+								settings.n_append.call();
+							}
+					}
+				});
+			}
+		}
+		
+		//Left and Right (Horizontal)
+		
+		else if(settings.direction=='horizontal'){
+			//if the object is document
+			if($(this).is(document)){
+				$(this).scroll(function(){
+					//Scroll right
+					if(left_position<$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollRight()==0){
+							$('body').css('width',$('body').width()*2);
+							$('body').children('.endless').css('float','left');
+							$('body').append($('body').html());
+							$('body').children('.endless:nth-child(2)').css('float','right');
+							$(this).scrollLeft(left_position);
+							append=true;
+							settings.append.call();
+						}
+						else if($(this).scrollLeft()>=width){
+							$('body').children('.endless:first-child').remove();
+							$('body').css('width',$('body').width()/2);
+							$(this).scrollLeft(1);
+							left_position=1;
+							prepend=false;
+							settings.n_prepend.call();
+						}
+					}
+					//Scroll left
+					else if(left_position>=$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollLeft()==0){
+							$('body').css('width',$('body').width()*2);
+							$('body').children('.endless').css('float','right');
+							$('body').prepend($('body').html());
+							$('body').children('.endless:first-child').css('float','left');
+							$(this).scrollLeft(width);
+							left_position=$(this).scrollLeft();
+							prepend=true;
+							settings.prepend.call();
+						}
+						else if($(this).scrollRight()>=width){
+							$('body').children('.endless:last-child').remove();
+							$('body').css('width',$('body').width()/2);
+							$(this).scrollLeft(width-$(window).width()-1);
+							left_position=$(this).scrollLeft();
+							append=false;
+							settings.n_append.call();
+						}
+					}
+				});
+			}
+			//else -> take the parent for scroll position 
+			else{
+				$(this).parent().scroll(function(){
+					//Scroll right
+					if(left_position<$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollRight()==0){
+							$(this).children().css('width',$(this).children().width()*2);
+							$(this).children().children('.endless').css('float','left');
+							$(this).children().append($(this).children().html());
+							$(this).children().children('.endless:nth-child(2)').css('float','right');
+							$(this).scrollLeft(left_position);
+							append=true;
+							settings.append.call();
+						}
+						else if($(this).scrollLeft()>=width){
+							$(this).children().children('.endless:first-child').remove();
+							$(this).children().css('width',$(this).children().width()/2);
+							$(this).scrollLeft(1);
+							left_position=1;
+							prepend=false;
+							settings.n_prepend.call();
+						}
+					}
+					//Scroll left
+					else if(left_position>=$(this).scrollLeft()){
+						left_position=$(this).scrollLeft();
+						if($(this).scrollLeft()==0){
+							$(this).children().css('width',$(this).children().width()*2);
+							$(this).children().children('.endless').css('float','right');
+							$(this).children().prepend($(this).children().html());
+							$(this).children().children('.endless:first-child').css('float','left');
+							$(this).scrollLeft(width);
+							left_position=$(this).scrollLeft();
+							prepend=true;
+							settings.prepend.call();
+						}
+						else if($(this).scrollRight()>=width){
+							$(this).children().children('.endless:last-child').remove();
+							$(this).children().css('width',$(this).children().width()/2); 
+							$(this).scrollLeft(width-1);
+							left_position=width-1;
+							append=false;
+							settings.n_append.call();
+						}
+					}
+				});
+			}
+		}
+		
+		return this;
+		
+	}
+})(jQuery);
