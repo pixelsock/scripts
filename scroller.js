e2bbfe829cabb928c985abd1b6346348bd0fd10a
pixelsock <nick@pixelsock.com>
2019-01-23 12:00:05 -0500

    Update scroller.js
    changed class activator

--- scroller.js
+++ scroller.js
@@ -3,37 +3,36 @@
         lastScrollTop : 0,
         direction: null,
     };
-    $.fn.lemniscate = function(options) {
-        console.log('----------', this.selector);
+    $.fn.infinite = function(options) {
         var defaults = {
-            container : this.selector,
+            container : '.js-loop',
         };
 
         var o = $.extend( {}, defaults, options );
-        $.fn.lemniscate._o = o;
+        $.fn.infinite._o = o;
         startLoop = function(){
             $(window).on('scroll',  handler.scroll);
-            $.fn.lemniscate.clone();
+            $.fn.infinite.clone();
         };
-        $.fn.lemniscate.clone = function(){
+        $.fn.infinite.clone = function(){
             var loop_clone = $(o.container).clone();
             $(o.container).parent().prepend( loop_clone ).append( loop_clone );
             setTimeout(function(){
-                $.fn.lemniscate.startPosition();
+                $.fn.infinite.startPosition();
             }, 200);
         };
-        $.fn.lemniscate.startPosition = function(){
+        $.fn.infinite.startPosition = function(){
             var center = $(o.container+':nth-child(2)').offset().top;
             $(window).scrollTop(center); 
         };
 
 
 
-        $.fn.lemniscate.destroy = function(){
+        $.fn.infinite.destroy = function(){
             $(window).off('scroll');
             $(window).off('mousewheel');
         }
-        $.fn.lemniscate.debug = function(){
+        $.fn.infinite.debug = function(){
             console.log(defaults , options);
         };
 
@@ -42,26 +41,23 @@
 
     var handler = {
         scroll : function(){
-            var down_clone    = $($.fn.lemniscate._o.container+':last-child '),
+            var down_clone    = $($.fn.infinite._o.container+':last-child '),
                 win_height    = $(window).height(),
                 win_width     = $(window).width(),
                 winPos        = $(window).scrollTop(),
                 clone_top     = down_clone.offset().top,
-                loop_height   = $($.fn.lemniscate._o.container).parent().outerHeight(true);
+                loop_height   = $($.fn.infinite._o.container).parent().outerHeight(true);
 
 
                         if(win_height + winPos >= loop_height ){
                 var scroll_down_loop =  down_clone.outerHeight(true) - win_height + (win_width * .03); 
                 $(window).scrollTop(scroll_down_loop); 
             }else if(winPos <= 0 ){
-                var scroll_up_loop =  $($.fn.lemniscate._o.container).last().offset().top ; 
+                var scroll_up_loop =  $($.fn.infinite._o.container).last().offset().top ; 
                 $(window).scrollTop(scroll_up_loop); 
             }
 
         }
     };
-
-    if ( typeof module === "object" && typeof module.exports === "object" ) {
-        module.exports = $.fn.lemniscate;
-    }
+    module.exports = $.fn.infinite;
 }(jQuery));
